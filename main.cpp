c1752c3492f1b862654e6e3a164f29b74a53664f
Avi Mukesh <avimukesh10@gmail.com>
2026-06-20 21:12:33 +0100

    simple order struct

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "order.hpp"
+#include <iostream>
+
+int main() {
+    auto order = Order(1, 250, Side::BUY, 5, "BTCUSD");
+    std::cout << order;
+    return 0;
+}
---
+++ order.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include<iostream>
+#include<cstdint>
+#include<string>
+#include<utility>
+
+enum class Side {BUY, SELL};
+
+struct Order {
+    uint64_t id;
+    double price;
+    Side side;
+    int quantity;
+    std::string symbol;
+
+    Order(uint16_t id, double price, Side side, int quantity, std::string symbol) : 
+            id(id), price(price), side(side), quantity(quantity), symbol(std::move(symbol)) {}
+};
+
+std::ostream& operator<<(std::ostream & str, Order const & order) {
+    str << "ID: " << order.id << " Price: " << order.price;
+    return str;
+}
