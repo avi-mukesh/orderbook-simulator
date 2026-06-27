56373cf7dd24630ebbdedcbc3ae53141ed88cea6
Avi Mukesh <avimukesh10@gmail.com>
2026-06-27 13:40:20 +0100

    tests for IOC and FOK orders

--- tests.cpp
+++ tests.cpp
@@ -198,4 +198,58 @@ TEST_CASE("testing orderbook creation") {
         CHECK(book.trade_log().size() == 5);
         CHECK(book.best_bid() == std::nullopt);
     }
+
+    SUBCASE("IOC order if partially filled, won't rest on the book") {
+        auto sell_id_1 = book.add_order(Order(1000, Side::SELL, 5, "BTCUSD"));
+        auto sell_id_2 = book.add_order(Order(1010, Side::SELL, 5, "BTCUSD"));
+
+        auto buy_id = book.add_order(Order(OrderType::IOC, 1200, Side::BUY, 7, "BTCUSD"));
+        
+        auto trade_log = book.trade_log();
+        CHECK(trade_log.size() == 2);
+        CHECK(trade_log[0].buy_id == buy_id);
+        CHECK(trade_log[0].sell_id == sell_id_1);
+        CHECK(trade_log[0].quantity == 5);
+        CHECK(trade_log[0].fill_price == 1000);
+
+        CHECK(trade_log[1].buy_id == buy_id);
+        CHECK(trade_log[1].sell_id == sell_id_2);
+        CHECK(trade_log[1].quantity == 2);
+        CHECK(trade_log[1].fill_price == 1010);
+
+        CHECK(book.best_bid() == std::nullopt);
+    }
+
+    SUBCASE("FOK order if can fill completely, gets filled") {
+        auto sell_id_1 = book.add_order(Order(1000, Side::SELL, 5, "BTCUSD"));
+        auto sell_id_2 = book.add_order(Order(1010, Side::SELL, 5, "BTCUSD"));
+
+        auto buy_id = book.add_order(Order(OrderType::FOK, 1200, Side::BUY, 7, "BTCUSD"));
+        
+        auto trade_log = book.trade_log();
+        CHECK(trade_log.size() == 2);
+        CHECK(trade_log[0].buy_id == buy_id);
+        CHECK(trade_log[0].sell_id == sell_id_1);
+        CHECK(trade_log[0].quantity == 5);
+        CHECK(trade_log[0].fill_price == 1000);
+
+        CHECK(trade_log[1].buy_id == buy_id);
+        CHECK(trade_log[1].sell_id == sell_id_2);
+        CHECK(trade_log[1].quantity == 2);
+        CHECK(trade_log[1].fill_price == 1010);
+        
+        CHECK(book.best_bid() == std::nullopt);
+    }
+
+    SUBCASE("FOK order if can't fill completely, doesn't rest on the book") {
+        auto sell_id_1 = book.add_order(Order(1000, Side::SELL, 5, "BTCUSD"));
+        auto sell_id_2 = book.add_order(Order(1010, Side::SELL, 5, "BTCUSD"));
+
+        auto buy_id = book.add_order(Order(OrderType::FOK, 1200, Side::BUY, 200, "BTCUSD"));
+        
+        auto trade_log = book.trade_log();
+        CHECK(trade_log.size() == 0);
+        
+        CHECK(book.best_bid() == std::nullopt);
+    }
 }
